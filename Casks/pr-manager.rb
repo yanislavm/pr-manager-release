dfd9c439b74e082f7efbf9b850ab2f4821357558
Yanislav Mihaylov <jany81@gmail.com>
2026-07-03 12:06:26 +0100

    Update pr-manager to 0.0.7

--- Casks/pr-manager.rb
+++ Casks/pr-manager.rb
@@ -1,11 +1,11 @@
 cask "pr-manager" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.0.6"
-  sha256 arm:   "5025f372f49413733b6b2e5ac4a815667d153a8c673e3d526922d66a82fc0e7d",
-         intel: "18a31e31994e12c7fd68280090c9759ef475fe42236fc97b8694d3bb83e7a62a"
+  version "0.0.7"
+  sha256 arm:   "e4a207635deb1222c7c4230c78706b9e267177beb03e8a908ce1a1addde8e1f3",
+         intel: "841b06aec1fde5ea8ce5cafe76132e94031522e6bd281545bdfbc261381bb5ee"
 
-  url "https://github.com/yanislavm/pr-manager-release/releases/download/v0.0.6/pr-manager-darwin-#{arch}.tar.gz"
+  url "https://github.com/yanislavm/pr-manager-release/releases/download/v0.0.7/pr-manager-darwin-#{arch}.tar.gz"
   name "PR Manager"
   desc "Terminal UI for monitoring GitHub pull requests"
   homepage "https://github.com/yanislavm/pr-manager"
